0a89722a41b59ef47f7f082525f2a24c99e508be
ekaterinasavcuk <heeykat@gmail.com>
2024-06-10 21:10:15 +0200

    2. Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,21 @@
+package ru.job4j.collection;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class UniqueText {
+    public boolean isEquals(String originText, String duplicateText) {
+        boolean result = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        check.addAll(Arrays.asList(origin));
+        for (String el: text) {
+            if (!check.contains(el)) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
