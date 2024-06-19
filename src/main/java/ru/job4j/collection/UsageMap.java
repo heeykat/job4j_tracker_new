4cb1653c416bd9c815b2cc0d2dd8b5fc229d5fe8
ekaterinasavcuk <heeykat@gmail.com>
2024-06-19 11:54:35 +0200

    0. Map, HashMap [#212652]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,6 +7,9 @@ public class UsageMap {
         HashMap<String, String> map = new HashMap<>();
         map.put("ivan@mail.ru", "Ivanov Ivan Ivanovich");
         map.put("ivan_ivan@mail.ru", "Ivanov Ivan");
+        map.put("123@gmail.com", "Petrov Petr");
+        map.put("345@gmail.com", "Petrov Ivan");
+        map.put("123@gmail.com", "Ivanov Petr");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
