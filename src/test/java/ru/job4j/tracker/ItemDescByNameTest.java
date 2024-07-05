318c0275e5d9f2666170faf7bc7aafa56f730209
ekaterinasavcuk <heeykat@gmail.com>
2024-07-05 16:58:17 +0200

    0. Сортировка [#10096]

---
+++ src/test/java/ru/job4j/tracker/ItemDescByNameTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.tracker;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemDescByNameTest {
+    @Test
+    public void whenSortDescByName() {
+        List<Item> listActual = new ArrayList<>() {{
+            add(new Item(1, "Abc"));
+            add(new Item(2, "Cbc"));
+            add(new Item(3, "Bbc"));
+        }};
+        List<Item> listExpected = new ArrayList<>() {{
+            add(new Item(2, "Cbc"));
+            add(new Item(3, "Bbc"));
+            add(new Item(1, "Abc"));
+        }};
+        Collections.sort(listActual, new ItemDescByName());
+        for (int i = 0; i < listActual.size(); i++) {
+            assertEquals(listExpected.get(i).getName(), listActual.get(i).getName());
+        }
+    }
+}
