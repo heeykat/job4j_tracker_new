bc39ad513ce20fd7f410100aa59418b1c84bd2b2
ekaterinasavcuk <heeykat@gmail.com>
2024-08-06 15:16:42 +0200

    6. Подсчет функции в диапазоне. [#24251]

---
+++ src/test/java/ru/job4j/function/FunctionCalculatorTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.function;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FunctionCalculatorTest {
+    @Test
+    void whenLinearFunctionThenLinearResults() {
+        FunctionCalculator function = new FunctionCalculator();
+        List<Double> result = function.diapason(5, 8, x -> 2 * x + 1);
+        List<Double> expected = Arrays.asList(11D, 13D, 15D);
+        assertThat(result).containsAll(expected);
+    }
+
+    @Test
+    void whenQuadraticFunctionThenQuadraticResults() {
+        FunctionCalculator function = new FunctionCalculator();
+        List<Double> result = function.diapason(1, 4, x -> 3 * x * x + 2 * x + 4);
+        List<Double> expected = Arrays.asList(9D, 20D, 37D);
+        assertThat(result).containsAll(expected);
+    }
+
+    @Test
+    void whenExponentialFunctionThenExponentialResults() {
+        FunctionCalculator function = new FunctionCalculator();
+        List<Double> result = function.diapason(1, 4, x -> 2 * (Math.pow(3, x)));
+        List<Double> expected = Arrays.asList(6D, 18D, 54D);
+        assertThat(result).containsAll(expected);
+    }
+}
