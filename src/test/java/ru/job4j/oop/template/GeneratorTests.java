2f92ead745187edbea83d7d3a0bf7da3251dc6d2
ekaterinasavcuk <heeykat@gmail.com>
2026-01-12 18:22:46 +0100

    4. Шаблонизатор. [#855]

---
+++ src/test/java/ru/job4j/oop/template/GeneratorTests.java
@@ -0,0 +1,57 @@
+package ru.job4j.oop.template;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+public class GeneratorTests {
+    Generator generator = new GeneratorImpl();
+
+    @Test
+    public void whenAllKeysProvidedThenTemplateIsFilled() {
+        String template = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> args = Map.of(
+                "name", "Petr Arsentev",
+                "subject", "you"
+        );
+
+        String result = generator.produce(template, args);
+        assertThat(result).isEqualTo("I am a Petr Arsentev, Who are you?");
+    }
+
+    @Test
+    public void whenOneOfTheKeysIsMissingThenException() {
+        String template = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> args = Map.of(
+                "name", "Petr Arsentev"
+        );
+        String result = generator.produce(template, args);
+        assertThatThrownBy(() -> generator.produce(template, args))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void whenContainsExtraKeysThenException() {
+        String template = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> args = Map.of(
+                "name", "Petr Arsentev",
+                "subject", "you",
+                "extra", "value"
+        );
+        String result = generator.produce(template, args);
+        assertThatThrownBy(() -> generator.produce(template, args))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void whenMapIsEmptyThenException() {
+        String template = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> args = Map.of();
+        String result = generator.produce(template, args);
+        assertThatThrownBy(() -> generator.produce(template, args))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
